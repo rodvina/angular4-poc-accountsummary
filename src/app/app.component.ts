c1ebf57876681fdce811ceb0b3ccc4b342b713af
rodney.odvina <rodvina@kemper.com>
2017-08-15 16:49:58 -0400

    Added ng-bootstrap and customer-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  successrate = 10;
+  inforate = 22;
+  warningrate = 78;
+  dangerrate = 5;
 }
---
+++ src/app/customer-list/customer-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-list',
+  templateUrl: './customer-list.component.html',
+  styleUrls: ['./customer-list.component.scss']
+})
+export class CustomerListComponent implements OnInit {
+
+  customers: string[] = [
+    'Rodney',
+    'Bob'
+  ];
+
+  constructor() {
+
+    // use service to make call to return data
+
+  }
+
+  ngOnInit() {
+  }
+
+}
